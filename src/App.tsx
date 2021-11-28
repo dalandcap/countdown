8eeea7237207cfb75424571394fdf3d73f367bb2
Daniel Filat <daniel.filat@reigncode.dev>
2021-11-28 10:45:38 +0200

    render time cell for each segment

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,11 @@ import TimeCell from './components/TimeCell/TimeCell';
 const END_TIME = '6 December 2021 07:14:52 GMT';
 
 function App() {
-  const [time, setTime] = useState(new Date(END_TIME));
-  console.log(intervalToDuration({start: time, end: Date.now()}));
+  const [time, setTime] = useState(intervalToDuration({start: new Date(END_TIME), end: Date.now()}));
 
   return (
     <div className="App">
-      <TimeCell value={60} label={i18next.t("days")}/>
+      {Object.entries(time).map(([segment,value]) => <TimeCell label={segment} value={value}/>)}
     </div>
   );
 }
