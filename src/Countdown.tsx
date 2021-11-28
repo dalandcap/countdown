f12ca15adf63d1858cdbe61fed9fcb28f71aa82b
Daniel Filat <daniel.filat@reigncode.dev>
2021-11-28 11:36:11 +0200

    update countdown on every second

--- src/Countdown.tsx
+++ src/Countdown.tsx
@@ -1,35 +1,51 @@
 import { intervalToDuration } from 'date-fns';
 import i18next from 'i18next';
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import TimeCell from './components/TimeCell/TimeCell';
 
 const END_TIME = '6 December 2021 07:14:52 GMT';
 
-
 const CellsContainer = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-/* compensate from the margin-left of the fisrt cell since selecting the first child is not possible in styled-components and passing the the index to the cell wpuld be too much interference */
-margin-left: -1.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* compensate from the margin-left of the fisrt cell since selecting the first child is not possible in styled-components and passing the the index to the cell wpuld be too much interference */
+  margin-left: -1.5rem;
 
-.time-cell {
-  margin-left: 1.5rem;
-}
+  .time-cell {
+    margin-left: 1.5rem;
+  }
 `;
 
 const Countdown: FC = () => {
-  const [time, setTime] = useState(intervalToDuration({start: new Date(END_TIME), end: Date.now()}));
+  const getCountdownDuration = () =>
+  intervalToDuration({ start: new Date(END_TIME), end: Date.now() });
+
+  const [time, setTime] = useState<Duration>(getCountdownDuration());
+
+  useEffect(() => {
+    const tick = setInterval(() => {
+      setTime(getCountdownDuration());
+    }, 1000);
+    return () => clearInterval(tick)
+  }, [setTime]);
 
   return (
     <CellsContainer>
-      {Object.entries(time).map(([segment,value]) => {
+      {Object.entries(time).map(([segment, value]) => {
         // the cells for the semgments from hours below should be shown always
-        const showCell = value || segment === 'seconds' || segment === 'minutes' || segment === 'hours'
-        return showCell ? <TimeCell key={segment} label={i18next.t(segment)} value={value}/> : null})}
+        const showCell =
+          value ||
+          segment === 'seconds' ||
+          segment === 'minutes' ||
+          segment === 'hours';
+        return showCell ? (
+          <TimeCell key={segment} label={i18next.t(segment)} value={value} />
+        ) : null;
+      })}
     </CellsContainer>
   );
-}
+};
 
 export default Countdown;
