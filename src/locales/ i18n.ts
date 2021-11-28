17819ad1fb732bbe721f8ee5d637c45d5446b914
Daniel Filat <daniel.filat@reigncode.dev>
2021-11-28 09:40:19 +0200

    set up i18n

---
+++ src/locales/ i18n.ts
@@ -0,0 +1,19 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import En from './en.json';
+import Ro from './ro.json';
+
+i18n.use(LanguageDetector).init({
+    lng: 'ro',
+    fallbackLng: 'ro',
+    resources: {
+        en: {
+            translation: En,
+        },
+        ro: {
+            translation: Ro,
+        },
+    },
+});
+
+export default i18n;
