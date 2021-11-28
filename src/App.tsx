82a79312f59a8ddf592fd4c9f07f70c859615d63
Daniel Filat <daniel.filat@reigncode.dev>
2021-11-28 10:25:56 +0200

    get initial time state from constant

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { intervalToDuration } from 'date-fns';
 import i18next from 'i18next';
-import React from 'react';
+import React, { useState } from 'react';
 import TimeCell from './components/TimeCell/TimeCell';
 
-const TIME_SEGMENTS = 'days';
+const END_TIME = '6 December 2021 07:14:52 GMT';
 
 function App() {
+  const [time, setTime] = useState(new Date(END_TIME));
+  console.log(intervalToDuration({start: time, end: Date.now()}));
+
   return (
     <div className="App">
       <TimeCell value={60} label={i18next.t("days")}/>
