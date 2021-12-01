14b113a697fc8f83d3a304356db3cd3faa4c1f25
Daniel Filat <daniel.filat@reigncode.dev>
2021-12-01 19:17:57 +0200

    use fixture for time

--- src/tests/Countdown.test.tsx
+++ src/tests/Countdown.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { END_TIME } from '../App';
 import { Countdown } from '../components';
 import '../locales/i18n';
 import i18n from '../locales/i18n';
+import { add } from 'date-fns';
 
 const time: Duration = {
   months: 3,
@@ -13,14 +13,16 @@ const time: Duration = {
   seconds: 11
 };
 
+const endTime = add(new Date(), time);
+
 test('renders countdown', () => {
-  render(<Countdown endTime={new Date(END_TIME)} />);
+  render(<Countdown endTime={new Date(endTime)} />);
   const countdownElement = screen.getByTestId('cells-container');
   expect(countdownElement).toBeInTheDocument();
 });
 
 test('time cells render and each shows the correct value', () => {
-  render(<Countdown endTime={new Date(END_TIME)} />);
+  render(<Countdown endTime={new Date(endTime)} />);
   const cellElements: HTMLElement[] = [];
   Object.entries(time).forEach(([unit, value]) => {
     if (value || unit === 'seconds' || unit === 'minutes' || unit === 'hours')
